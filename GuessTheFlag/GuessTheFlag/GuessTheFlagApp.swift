b3597a5d5b2d3cc1a90d8f4952d7b6a845eba2fd
Krisztian Nagy <krisztian@uxconsultancy.com>
2022-05-24 13:16:42 +0100

    init

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Krisztián Nagy on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
